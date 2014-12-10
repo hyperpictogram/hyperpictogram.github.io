a8f3f93889aa41d4c802779bae76ef91ac0cdab1
Hideto Ishibashi <hidetoi@gmail.com>
2014-12-10 17:58:24 +0900

    graceful degradation: JavaScript disabled UA

--- js/m17n.js
+++ js/m17n.js
@@ -1,4 +1,9 @@
 function switchLanguage(lang) {
+	changeDisplayLanguage(lang)
+	updateLanguageButtonSelection(lang)
+}
+
+function changeDisplayLanguage(lang) {
 	var elements = document.getElementsByClassName('pictogram-m17n')
 	for (var i = 0; i < elements.length; i++) {
 		var element = elements[i]
@@ -6,6 +11,18 @@ function switchLanguage(lang) {
 			element.style.display = 'none'
 		} else {
 			element.style.display = 'block'
+		}
+	}
+}
+
+function updateLanguageButtonSelection(lang) {
+	var buttons = document.getElementsByClassName('pictogram-m17n-switch')
+	for (var i = 0; i < buttons.length; i++) {
+		var button = buttons[i]
+		if (button.lang != lang) {
+			button.classList.remove("selected")
+		} else {
+			button.classList.add("selected")
 		}
 	}
 }
